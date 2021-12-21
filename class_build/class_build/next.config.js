2638cdc4e494bb0ba21c3db5ba30320c7d48d599
OhMinWook <minugi72@gmail.com>
2021-12-21 14:40:19 +0900

    Update

--- class_build/class_build/next.config.js
+++ class_build/class_build/next.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "minugi",
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/board": { page: "/board" },
+    "/404": { page: "/404" },
+  }),
 };
